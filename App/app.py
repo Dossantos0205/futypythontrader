c975704c44e1cc3a2934d3a5cf53c11d0ffc308e
Dossantos0205 <157335593+Dossantos0205@users.noreply.github.com>
2026-09-01 22:00:26 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
 
 st.title("Web App Football Data")
 
@@ -11,7 +12,6 @@ st.sidebar.header("Season")
 selected_season = st.sidebar.selectbox('Season',['2021/2022','2020/2021','2019/2020'])
 
 # WebScrapping Football Data
-
 def load_data(league, season):
 
   if selected_league  == 'England':
@@ -40,3 +40,11 @@ df = load_data(selected_league, selected_season)
 
 st.subheader("Dataframe: "+selected_league)
 st.dataframe(df)
+
+def filedownload(df):
+  csv = df.to_csv(index=False)
+  b64 = base64.b64encode(csv.encode()).decode()
+  href = f'<a href="data:file/csv;base64,{64}" download="Base_de_Dados.csv">Download CSV File</a>'
+  return href
+
+st.markdown(filedownload(df), unsafe_allow_html=True)
