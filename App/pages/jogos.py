a2c94cb5105d061ae6186051b7cdc40d08a153b1
Dossantos0205 <157335593+Dossantos0205@users.noreply.github.com>
2026-09-01 21:47:25 -0300

    Update jogos.py

--- App/pages/jogos.py
+++ App/pages/jogos.py
@@ -10,15 +10,15 @@ dia = st.date_input(
       "Data de Análise",
       date.today())
 
-def load_data_jogos():
+def load_data_jogos(dia):
   API_KEY = "cmsa0nii002b34dw4b8vfwqma"
-  DIA = date.today().isoformat()
+  DIA = dia.isoformat()
   url = f"https://futpythontrader.com.br/api/jogos-do-dia?date={DIA}&format=csv&api_key={API_KEY}"
 
   jogos_do_dia = pd.read_csv(url)
   # display(jogos_do_dia)
   return jogos_do_dia
 
-df_jogos = load_data_jogos()
+df_jogos = load_data_jogos(dia)
 
 st.dataframe(df_jogos)
